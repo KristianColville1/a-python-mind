0f2002ff69ef8378dfe3ad51e78f27e0018a6653
KristianColville1 <kristiancolville1@gmail.com>
2022-04-21 21:11:49 +0100

    Create a simple bandname generator

---
+++ 100-days-of-code/days-1-14/day-one/bandname-generator.py
@@ -0,0 +1,16 @@
+"""Creates a simple bandname for a user"""
+
+print(
+    'Welcome to the bandname generator!\n\n'
+)
+city = input(
+    'Enter the name of the town you grew up in: '
+    )
+
+pet = input(
+    '\nEnter the name of your first pet: '
+)
+
+print(
+    '\n\nYour band name is: ' + city + ' ' + pet
+)
